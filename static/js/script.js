ec4fc23906c6f732ea722b76648fd5b0164419a9
prafulSawarkar <prafulsavarkarr77889900@gmail.com>
2023-08-14 14:47:02 +0530

    alert action added delete action and update

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,20 @@ logOut= () => {
     console.log("nothig")
    }
 }
+
+var myDiv = document.getElementById('myNotification');
+
+  
+
+setTimeout(function() {
+    if (myDiv) {
+        myDiv.classList.add('fade-in');
+        setTimeout(function() {
+            myDiv.classList.remove('fade-in');
+            myDiv.classList.add('fade-out');
+            setTimeout(function() {
+                myDiv.remove();
+            }, 100); // Wait for the fade-out animation to complete before removing the div
+        }, 3000); // Wait for 4 seconds for the div to fade-in before starting fade-out
+    }
+}, 0000); // 1 second delay
